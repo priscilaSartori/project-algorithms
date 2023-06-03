6a237ff545f766657c2a19f99c3d604142246a0d
Priscila <priscilasartori2@gmail.com>
2023-06-03 20:23:30 -0300

    feat: Implementando  is_palindrome_recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,14 @@
+# 3.1 - Retorne True se a palavra passada por parâmetro for um palíndromo;
+# 3.2 - Retorne False se a palavra passada por parâmetro não for um palíndromo;
+# 3.3 - Retorne False se nenhuma palavra for passada por parâmetro.
+
+
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not word or word[low_index] != word[high_index]:
+        return False
+    if low_index >= high_index:
+        return True
+    if word[low_index] == word[high_index]:
+        low_index += 1
+        high_index -= 1
+        return is_palindrome_recursive(word, low_index, high_index)
